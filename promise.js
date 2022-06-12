433a75cedb3f46acff853d55ae049b060cf8c907
NaomyAckerman <ackermannaomy@gmail.com>
2022-06-12 20:24:18 +0700

    Tantangan 07

--- promise.js
+++ promise.js
@@ -1,8 +1,21 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (sentimen) => {
+	const ixxMovies = await promiseTheaterIXX();
+	const vgcMovies = await promiseTheaterVGC();
+	const movies = [...vgcMovies, ...ixxMovies];
+	return new Promise((resolve, reject) => {
+		if (movies.length >= 1) {
+			// Jika movie tersedia
+			resolve(movies.filter((movie) => movie.hasil === sentimen).length);
+		} else {
+			// Jika movie kosong
+			reject('Movie tidak tersedia');
+		}
+	});
+};
 
 module.exports = {
-  promiseOutput,
+	promiseOutput,
 };
